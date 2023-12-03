b97a196c715b4787ecc3a588a2d04f9ad5a50af7
Tetiana-Antoniuk <133028456+Tetiana-Antoniuk@users.noreply.github.com>
2023-12-03 20:20:57 +0200

    hw04 done

--- src/main.js
+++ src/main.js
@@ -127,10 +127,10 @@ function createMarkup(data) {
           <a class="photo-card__link" href="${largeImageURL}">
             <img class="photo-card__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
             <div class="info">
-              <p class="info-item"><b>Likes ${likes}</b></p>
-              <p class="info-item"><b>Views ${views}</b></p>
-              <p class="info-item"><b>Comments ${comments}</b></p>
-              <p class="info-item"><b>Downloads ${downloads}</b></p>
+              <p class="info-item"><b>Likes</b> <span class="info-item__wrapper">${likes}</span></p>
+              <p class="info-item"><b>Views</b> <span class="info-item__wrapper">${views}</span></p>
+              <p class="info-item"><b>Comments</b> <span class="info-item__wrapper">${comments}</span></p>
+              <p class="info-item"><b>Downloads</b> <span class="info-item__wrapper">${downloads}</span></p>
             </div>
           </a>
         </div>`
